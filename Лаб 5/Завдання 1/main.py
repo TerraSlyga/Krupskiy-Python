62aae24dfd1ede67d32953df65bd4c97f7b3747c
Sasha <96521943+TerraSlyga@users.noreply.github.com>
2023-09-27 15:42:16 +0300

    Лаб 5(виправлено)

--- Лаб 5/Завдання 1/main.py
+++ Лаб 5/Завдання 1/main.py
@@ -1,14 +1,8 @@
-def findMin(arr:list):
-    minimum: int = 999999999
-    for i in arr:
-        if i < minimum:
-            minimum = i
-    return minimum
 
 if __name__ == '__main__':
-    n: int = int(input("Введіть кількість елемнентів:\n"))
+    n = int(input("Введіть кількість елемнентів:\n"))
     arr = []
     for i in range(n):
         print(f"Введіть елемент {i}:")
         arr.append(int(input()))
-    print(f'Мінімальне число: {findMin(arr)}')
+    print(f'Мінімальне число: {min(arr)}')
