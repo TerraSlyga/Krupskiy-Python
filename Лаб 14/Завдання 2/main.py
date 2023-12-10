c500a5a51a05571168617ba1daaeea9a854174d1
Sasha <96521943+TerraSlyga@users.noreply.github.com>
2023-12-10 11:06:36 +0200

    Лаб 14(Виправлено)

--- Лаб 14/Завдання 2/main.py
+++ Лаб 14/Завдання 2/main.py
@@ -1,53 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-         2017, 2018, 2019, 2020, 2021, 2022]
-ukraine_datas = [22824874, 22682516, 22529601, 22365498, 22455476, 22353775, 22246256, 22140771, 22037073, 21940939,
-                 21834999, 21670714, 21563197, 21453122, 21345004, 21244133, 21148251, 20585937, 20285701, 20285701]
-usa_datas = [148886198, 150113013, 152044690, 154154002, 155503665, 157246174, 157377606, 157357516, 157443452,
-             158452840, 158809862, 159540139, 160644681, 162448669, 163971527, 165307010, 167100511, 165641653,
-             166189867, 169229171]
-np.array(np.int_(years))
+# Зчитуємо дані з CSV файлу
+csv_file = "data.csv"
 
-np.array(ukraine_datas)
+with open(csv_file, newline='') as file:
+    reader = csv.reader(file)
+    header = next(reader)  # Читаємо заголовок
 
-np.array(usa_datas)
+    # Витягуємо дані з рядка
+    years, ukraine_datas, usa_datas = zip(*[(int(row[0]), int(row[1]), int(row[2])) for row in reader])
 
+# Конвертуємо списки у numpy arrays
+years = np.array(years)
+ukraine_datas = np.array(ukraine_datas)
+usa_datas = np.array(usa_datas)
+
+# Графік 1
 plt.plot(years, ukraine_datas, label='Ukraine', color="blue")
-
 plt.plot(years, usa_datas, label='USA', color="yellow")
-
-plt.title('Діаграма робочої сили    ', fontsize=12)  # назва графіка
-
-plt.xlabel('Рік', fontsize=10, color='black')  # позначення вісі абсцис
-
-plt.ylabel('Значення', fontsize=10, color='black')  # позначення вісі ординат
-
+plt.title('Діаграма робочої сили', fontsize=12)
+plt.xlabel('Рік', fontsize=10, color='black')
+plt.ylabel('Значення', fontsize=10, color='black')
 plt.legend()
-
 plt.grid(True)
-
 plt.show()
 
-county = input("Введіть назву країни для побудови діаграми(Україна або США): ")
-
+# Графік 2
+county = input("Введіть назву країни для побудови діаграми (Україна або США): ")
 fig, ax = plt.subplots()
 
 if county.lower() == "україна":
     ax.bar(years, ukraine_datas)
     plt.title("Діаграма робочої сили України", fontsize=15)
-if county.lower() == "сша":
+elif county.lower() == "сша":
     ax.bar(years, usa_datas)
     plt.title("Діаграма робочої сили США", fontsize=15)
-plt.xlabel('Рік', fontsize=10, color='black')  # позначення вісі абсцис
 
-plt.ylabel('Значення', fontsize=10, color='black')  # позначення вісі ординат
-
+plt.xlabel('Рік', fontsize=10, color='black')
+plt.ylabel('Значення', fontsize=10, color='black')
 ax.set_facecolor('seashell')
-
-fig.set_figwidth(19)  # ширина Figure
-
-fig.set_figheight(12)  # висота Figure
-
+fig.set_figwidth(19)
+fig.set_figheight(12)
 plt.show()
